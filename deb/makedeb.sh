7be6254abe6217e45755b1883672307f3af64aeb
pioz <epilotto@gmx.com>
2017-10-31 13:34:04 +0100

    add deb dir to build a deb package

---
+++ deb/makedeb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ARCHS="i386 amd64"
+echo $ARCHS | grep "$ARCH" > /dev/null
+if [ $? != 0 ]; then
+  echo "You need to set a valid $ARCH variable (i386|amd64)"
+  exit 2
+fi
+
+DEBEMAIL="epilotto@gmx.com"
+DEBFULLNAME="Enrico Pilotto"
+export DEBEMAIL DEBFULLNAME
+
+NAME="bulldog"
+VERSION=`cat VERSION`
+PKGDIR=$NAME-$VERSION
+
+rm -rf $PKGDIR
+rm -rf ${NAME}_$VERSION*
+
+mkdir -p $PKGDIR/usr/bin
+cp ${NAME}_$ARCH $PKGDIR/usr/bin/$NAME
+cp -r etc $PKGDIR
+cd $PKGDIR
+dh_make -s --createorig --copyright=gpl3 -y
+cd ..
+rm $PKGDIR/debian/README*
+cp $NAME.1 $NAME.manpages changelog copyright install postinst postrm VERSION $PKGDIR/debian
+cp control_$ARCH $PKGDIR/debian/control
+cd $PKGDIR
+dpkg-buildpackage -us -uc
+cd ..
+debc ${NAME}_$VERSION-1_$ARCH.changes
+lintian ${NAME}_$VERSION-1_$ARCH.deb
